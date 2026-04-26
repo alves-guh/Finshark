9d1f806dea19b31fddb7f52ebf0f491d0a95fca0
alves-guh <alvesdasilvagustavo070@gmail.com>
2026-04-26 20:32:39 -0300

    Vídeo 6

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,22 @@
-import React, { JSX, useState } from 'react'
+import React, { ChangeEvent, JSX, useState, SyntheticEvent } from 'react'
 
 type Props = {}
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
     const [search, setSearch] = useState<string>("");
   
-    const onclick = (e: any) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
+        console.log(e);
+    };
+
+    const onClick = (e: SyntheticEvent) => {
         console.log(e);
     };
     return (
     <div>
-        <input value={search} onChange={(e) => onclick(e)}> </input>
+        <input value={search} onChange={(e) => handleChange(e)}/>
+        <button onClick={(e) => onClick(e)}/>
     </div>
   );
   
