50df7384f110a9a2976908a395878ddfede1b81a
alves-guh <alvesdasilvagustavo070@gmail.com>
2026-04-27 18:11:55 -0300

    Vídeo 9

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,27 @@ import { useState, ChangeEvent, SyntheticEvent } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
   
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
     };
 
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e);
+    const onClick = async (e: SyntheticEvent) => {
+        const result = await searchCompanies(search);
+        if(typeof result === "string"){
+          setServerError(result);
+        }else if(Array.isArray(result.data.data)){
+          setSearchResult(result.data.data);
+        }
+        console.log(searchResult);
     };
   return (
     <div className="App">
