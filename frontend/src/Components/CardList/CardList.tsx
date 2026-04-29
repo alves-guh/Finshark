3f2e21ae8b0619859f891369de4ea6b940e53404
alves-guh <alvesdasilvagustavo070@gmail.com>
2026-04-29 19:30:03 -0300

    Vídeo 15

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,6 +1,6 @@
-import React, { JSX, SyntheticEvent } from 'react'
-import Card from '../Card/Card'
-import { CompanySearch } from '../../company'
+import React, { JSX, SyntheticEvent } from "react";
+import Card from "../Card/Card";
+import { CompanySearch } from "../../company";
 import { v4 as uuidv4 } from "uuid";
 
 interface Props {
@@ -8,16 +8,29 @@ interface Props {
   onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const CardList: React.FC<Props> = ({searchResults, onPortfolioCreate}: Props): JSX.Element => {
-  return <>
-  {searchResults.length > 0 ? (
-    searchResults.map((result) => {
-      return <Card id={result.symbol} key={uuidv4()} searchResult={result} onPortfolioCreate={onPortfolioCreate}/>
-    })
-  ): (
-    <h1>No results</h1>
-  )}</>
-  
-}
-
-export default CardList
+const CardList: React.FC<Props> = ({
+  searchResults,
+  onPortfolioCreate,
+}: Props): JSX.Element => {
+  return (
+    <div>
+      {searchResults.length > 0 ? (
+        searchResults.map((result) => {
+          return (
+            <Card
+              id={result.symbol}
+              key={uuidv4()}
+              searchResult={result}
+              onPortfolioCreate={onPortfolioCreate}
+            />
+          );
+        })
+      ) : (
+        <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+          No results!
+        </p>
+      )}
+    </div>
+  );
+};
+export default CardList;
