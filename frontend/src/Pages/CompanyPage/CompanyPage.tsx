8174a53088aca2363b52178458145a4b40fa200c
alves-guh <alvesdasilvagustavo070@gmail.com>
2026-05-02 17:41:21 -0300

    Vídeo 18

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
 import { CompanyProfile } from "../../company";
 import { useParams } from "react-router";
 import { getCompanyProfile } from "../../api";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
+import { useState, useEffect } from "react";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
   const [company, setCompany] = useState<CompanyProfile>();
+  const [activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(() => {
     const getProfileInit = async () => {
@@ -20,7 +24,12 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div className="company-profile-container">{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard>
+            <Tile title="Company Name" subTitle={company.companyName} />
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company Not Found!</div>
       )}
