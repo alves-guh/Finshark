826323bcdf7bda2e54a208b9ab87f87a301b07de
alves-guh <alvesdasilvagustavo070@gmail.com>
2026-05-05 22:06:48 -0300

    Vídeo 28

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,10 @@
+import React, { useEffect, useState } from "react";
 import { CompanyProfile } from "../../company";
 import { Link, useParams } from "react-router";
 import { getCompanyProfile } from "../../api";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 import Tile from "../../Components/Tile/Tile";
-import { useState, useEffect } from "react";
 import Spinner from "../../Components/Spinner/Spinner";
 import CompFinder from "../../Components/CompFinder/CompFinder";
 import TenKFinder from "../../Components/TenKFinder/TenKFinder";
@@ -13,6 +13,7 @@ interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
+
   const [company, setCompany] = useState<CompanyProfile>();
 
   useEffect(() => {
@@ -30,9 +31,9 @@ const CompanyPage = (props: Props) => {
           <Sidebar />
           <CompanyDashboard ticker={ticker!}>
             <Tile title="Company Name" subTitle={company.companyName} />
-            <Tile title="Price" subTitle={company.price.toString()} />
+            <Tile title="Price" subTitle={"$" + company.price.toString()} />
+            <Tile title="DCF" subTitle={"$" + company.dcf.toString()} />
             <Tile title="Sector" subTitle={company.sector} />
-            <Tile title="Market Cap" subTitle={company.mktCap.toString()} />
             <CompFinder ticker={company.symbol} />
             <TenKFinder ticker={company.symbol} />
             <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
