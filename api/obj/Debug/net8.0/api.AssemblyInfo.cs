da666c5463e5a9abc920eeb7d5ae77c4b873174a
alves-guh <alvesdasilvagustavo070@gmail.com>
2026-05-14 19:53:11 -0300

    Vídeo 60

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7aee1570737dec4a06eb2c3ac2fd0f9caf3adf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5d311c6bc68b66cdf4365514162232fc3c42f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
